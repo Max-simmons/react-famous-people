2206f5a54edd7138813538a8e82b78bd5d6f39c4
Max Simmons <maxsimmons20@gmail.com>
2023-04-26 09:38:08 -0500

    got the names on the DOM

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const peopleRouter = require('./routes/people.router.js');
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json());
@@ -12,6 +12,6 @@ app.use(express.static('build'));
 app.use('/people', peopleRouter);
 
 /** ---------- START SERVER ---------- **/
-app.listen(PORT,  () => {
+app.listen(PORT, () => {
     console.log('Listening on port: ', PORT);
 });
--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './FamousSection.css';
+import axios from 'axios';
+// import { response } from 'express';
 
 function FamousSection() {
   let [famousPersonName, setPersonName] = useState('');
@@ -7,39 +9,76 @@ function FamousSection() {
   let [famousPeopleArray, setPeopleArray] = useState([]);
 
   // TODO: on load, call the fetchPeople() function
+  useEffect(() => {
+    fetchPeople();
+  }, [])
 
   const fetchPeople = () => {
+
+    axios({
+      method: 'GET',
+      url: '/people'
+    }).then((response) => {
+      console.log('IN GET');
+      const famousPeople = response.data;
+      setPeopleArray(famousPeople)
+    }).catch((error) => {
+      console.log('error here', error);
+
+    })
     // TODO: fetch the list of people from the server
   }
 
   const addPerson = (evt) => {
     evt.preventDefault();
     console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
-    
+
     // TODO: create POST request to add this new person to the database
 
-    // HINT: the server is expecting a person object 
-    //       with a `name` and a `role` property
-  
+    axios({
+      method: 'POST',
+      url: '/people',
+      data: {
+        name: famousPersonName,
+        role: famousPersonRole
+      }
+    }).then((response) => {
+      setPersonName('');
+      setPersonRole('');
+      fetchPeople();
+    }).catch((error) => {
+      console.log('error in POST')
+    })
   }
 
-    return (
-      <section className="new-person-section">
-        <form onSubmit={addPerson}>
-          <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={e => setPersonName(e.target.value)} />
-          <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
-          <button type="submit">Done</button>
-        </form>
-        <p>
-          {famousPersonName} is famous for "{famousPersonRole}".
-        </p>
-        <ul>
-          {/* TODO: Render the list of famous people */}
-        </ul>
-      </section>
-    );
+  // HINT: the server is expecting a person object 
+  //       with a `name` and a `role` p
+
+
+  return (
+    <section className="new-person-section">
+      <form onSubmit={addPerson}>
+        <label htmlFor="name-input">Name:</label>
+        <input id="name-input" onChange={e => setPersonName(e.target.value)} />
+        <label htmlFor="role-input">Famous for:</label>
+        <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
+        <button type="submit">Done</button>
+      </form>
+      <p>
+        {famousPersonName} is famous for "{famousPersonRole}".
+      </p>
+      <ul>
+        {
+          famousPeopleArray.map((person) => {
+            return (
+              <li key={person.id}>{person.name} is known for {person.role}</li>
+            )
+          })
+        }
+
+      </ul>
+    </section>
+  );
 }
 
 export default FamousSection;
