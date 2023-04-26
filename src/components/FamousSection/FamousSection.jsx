a090d73e957a4157893093a154cc637208fbd0e1
Max Simmons <maxsimmons20@gmail.com>
2023-04-26 09:46:07 -0500

    input fields are clear

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -59,9 +59,9 @@ function FamousSection() {
     <section className="new-person-section">
       <form onSubmit={addPerson}>
         <label htmlFor="name-input">Name:</label>
-        <input id="name-input" onChange={e => setPersonName(e.target.value)} />
+        <input id="name-input" value= {famousPersonName} onChange={e => setPersonName(e.target.value)} />
         <label htmlFor="role-input">Famous for:</label>
-        <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
+        <input id="role-input" value= {famousPersonRole} onChange={e => setPersonRole(e.target.value)} />
         <button type="submit">Done</button>
       </form>
       <p>
